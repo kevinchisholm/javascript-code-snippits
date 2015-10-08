bbb0a930c9d3333d12d00f7f3988f61e14d2ee0c
Kevin Chisholm <kevin212@pobox.com>
2015-10-08 07:08:01 -0400

    add global application

---
+++ native-javascript-snippits/global-application.js
@@ -0,0 +1,19 @@
+(function(_win, $){
+
+    'use strict';
+
+    /*global window: false */
+
+    var utils = {};
+
+    utils.init = function(){
+        //initialize the applcation
+    };
+
+    utils.start = function(){
+        //start the application
+    };
+
+    //initialize the applcation
+    utils.init();
+})(window, window.jQuery);
