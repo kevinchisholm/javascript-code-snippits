9d051307d3597c6ff6d18c5e688281115cd4a98c
Kevin Chisholm <kevin212@pobox.com>
2015-10-18 11:23:10 -0400

    add comment-templates.js

---
+++ native-javascript-snippits/comment-templates.js
@@ -0,0 +1,15 @@
+/**
+* Description...
+* Description...
+*
+* @param NAME (required/optional) Description....
+* that is a valid Cozi recipe ID
+*
+* @param NAME (required/optional) Description....
+* that is a valid Cozi recipe ID
+*
+* @return undefined
+*/
+function XXXX(){
+    //
+};
