2610c9231575b68463646a31278bbc9e3c12ae10
Kevin Chisholm <kevin212@pobox.com>
2015-11-07 19:54:54 -0500

    clean up code and add comments

--- native-javascript-snippits/add-css-file.js
+++ native-javascript-snippits/add-css-file.js
@@ -1,7 +1,16 @@
-var uniqueNum = new Date().getTime();
-var cssURL = 'style.css?ver=' + uniqueNum;
-var cssElement=document.createElement('link')
-cssElement.setAttribute('rel','stylesheet')
-cssElement.setAttribute('type','text/css')
-cssElement.setAttribute('href', cssURL);
-document.getElementsByTagName("head")[0].appendChild(cssElement);
+(function(){
+    //create a unique number
+    var uniqueNum = new Date().getTime(),
+        //create the URL
+        cssURL = 'style.css?ver=' + uniqueNum,
+        //create the link element
+        cssElement = document.createElement('link');
+
+    //set the attributes on the link element
+    cssElement.setAttribute('rel','stylesheet');
+    cssElement.setAttribute('type','text/css');
+    cssElement.setAttribute('href', cssURL);
+
+    //inject the link element
+    document.getElementsByTagName("head")[0].appendChild(cssElement);
+})();
