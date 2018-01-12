9b4863f3e86ea538a0a2b852dac83ba38e7b8423
Kevin Chisholm <kevin212@pobox.com>
2018-01-12 11:39:54 -0500

    Add component_template.component.ts

---
+++ angular-snippits/angular-2.x/component_template.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'SELECTOR_NAME_HERE',
+  templateUrl: './component_template.html',
+  styles: [ require('./component_template.scss') ]
+})
+export class ComponentTemplate {
+
+}
