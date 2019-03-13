791d2f28328c21f1facb796b0b18c8284f2fb2f0
Kevin Chisholm <kevin@kevinchisholm.com>
2019-03-13 09:51:22 -0400

    Create console-statements.js

---
+++ native-javascript-snippits/console-statements.js
@@ -0,0 +1,10 @@
+
+// basic -----------
+console.log();
+console.warn();
+console.dir();
+
+// with templating -----------
+console.warn(`XXXX ${foo} / ${bar} / ${baz}`);
+console.log(`XXXX ${foo} / ${bar} / ${baz}`);
+
