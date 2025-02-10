ef47b0fee981b2cdccc24a9b743f40afc2da0e62
Kevin Chisholm <kevin@kevinchisholm.com>
2025-02-10 10:07:15 -0500

    Add common combinations

--- native-javascript-snippits/console-statements.js
+++ native-javascript-snippits/console-statements.js
@@ -3,8 +3,15 @@
 console.log();
 console.warn();
 console.dir();
+console.log(JSON.stringify());
 
 // with templating -----------
-console.warn(`XXXX ${foo} / ${bar} / ${baz}`);
-console.log(`XXXX ${foo} / ${bar} / ${baz}`);
+console.warn(`XXXX ${XXXX} / ${XXXX} / ${XXXX}`);
+console.log(`XXXX ${XXXX} / ${XXXX} / ${XXXX}`);
 
+// common combinations -----------
+console.warn();
+console.dir();
+
+console.warn();
+console.log(JSON.stringify());
