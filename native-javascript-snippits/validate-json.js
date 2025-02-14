4eef5f4592c518ff13a9674a86d8a11900476a87
Kevin Chisholm <kevin@kevinchisholm.com>
2025-02-14 05:30:42 -0500

    Create validate-json.js

---
+++ native-javascript-snippits/validate-json.js
@@ -0,0 +1,12 @@
+function validateJson(jsonString) {
+	try {
+		JSON.parse(jsonString);
+		console.warn('JSON IS Valid!');
+	} catch (error) {
+		console.error('JSON is NOT Valid!', error);
+	}
+}
+
+const json = '{"foo" : "bar"}';
+
+validateJson(json); 
